8b328cb78001eba7ecfb4f13a71f10949b313dfd
kiranjeetkour144@gmail.com <kiranjeet kour>
2025-07-11 17:34:28 +0530

    Add Navbar component with responsive layout and RainbowButton for navigation

--- src/components/UI/Navbar.tsx
+++ src/components/UI/Navbar.tsx
@@ -1,11 +1,19 @@
+"use client"
 import Image from 'next/image'
 import React from 'react'
+import { RainbowButton } from '../magicui/rainbow-button'
+import { useRouter } from 'next/navigation'
 
 function Navbar() {
+  const router = useRouter()
   return (
-      <div className='bg-white/30 backdrop-blur-md flex items-center px-12 justify-left p-3'>
-          <Image alt="logo" src="/logo.png" height={100} width={120} />
-      </div>
+      <div className='bg-white/30 backdrop-blur-md flex items-center px-12 justify-between p-3'>
+      <Image alt="logo" src="/logo.png" height={100} width={120} />
+      <RainbowButton
+        onClick={() => router.push('/sip')}  >
+        Sip
+      </RainbowButton>
+    </div>
   )
 }
 
